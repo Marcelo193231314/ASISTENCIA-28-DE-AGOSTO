1fe88abdf6d119ff36dda47198959d0760530024
Marcelo193231314 <marcelo.silva.am06@gmail.com>
2025-08-28 15:15:44 -0600

    Update CalculadoraRecursiva.java

--- CalculadoraRecursiva.java
+++ CalculadoraRecursiva.java
@@ -30,4 +30,20 @@ public class CalculadoraRecursiva {
         return cadena.charAt(cadena.length() - 1) +
                 invertirCadena(cadena.substring(0, cadena.length() - 1));
     }
+
+    public int contarVocales(String cadena) {
+        if (cadena == null || cadena.isEmpty()) {
+            return 0;
+        }
+
+        char primerCaracter = Character.toLowerCase(cadena.charAt(0));
+        int esVocal = esVocal(primerCaracter) ? 1 : 0;
+
+        return esVocal + contarVocales(cadena.substring(1));
+    }
+
+    private boolean esVocal(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
+                c == 'á' || c == 'é' || c == 'í' || c == 'ó' || c == 'ú';
+    }
 }
