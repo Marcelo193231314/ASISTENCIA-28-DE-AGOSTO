ab066e3d1648d16b20234b7d0f05c9daf8f860cc
Marcelo193231314 <marcelo.silva.am06@gmail.com>
2025-08-28 15:14:28 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,1 @@
-public class Main {
-    public static void main(String[] args) {
-        CalculadoraRecursiva calculadora = new CalculadoraRecursiva();
-        MenuPrincipal menu = new MenuPrincipal(calculadora);
-        menu.ejecutar();
-    }
-}
+
